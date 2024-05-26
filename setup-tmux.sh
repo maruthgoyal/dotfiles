7fcc8eb545042e5de32a8102e2bda1fe1f077e72
Maruth Goyal <maruthgoyal@gmail.com>
2024-05-26 16:51:01 -0700

    fix out

--- setup-tmux.sh
+++ setup-tmux.sh
@@ -2,10 +2,10 @@
 
 set -euxo pipefail
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-touch "$HOME/tmux.conf"
+touch "$HOME/.tmux.conf"
 
 
 # Output source commands to target vimrc
-echo "source ${SCRIPT_DIR}/tmux/tmux.conf" > "$HOME/tmux.conf"
+echo "source ${SCRIPT_DIR}/tmux/tmux.conf" > "$HOME/.tmux.conf"
 
 echo "Successfully wrote source commands to ~/tmux.conf"
