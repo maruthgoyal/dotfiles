981f4dfe63acc6bca7dd854871fd8c93c6d3d5fa
Maruth Goyal <maruthgoyal@gmail.com>
2024-05-26 16:48:39 -0700

    add tmux, make relative

---
+++ setup-tmux.sh
@@ -0,0 +1,11 @@
+#! /usr/bin/env bash
+
+set -euxo pipefail
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+touch "$HOME/tmux.conf"
+
+
+# Output source commands to target vimrc
+echo "source ${SCRIPT_DIR}/tmux/tmux.conf" > "$HOME/tmux.conf"
+
+echo "Successfully wrote source commands to ~/tmux.conf"
--- setup-vim.sh
+++ setup-vim.sh
@@ -3,8 +3,9 @@
 set -euxo pipefail
 touch "$HOME/.vimrc"
 
-vimrc_path="/Users/maruthgoyal/dotfiles/vim/vimrc"
-coc_config_path="/Users/maruthgoyal/dotfiles/vim/coc-config.vim"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+vimrc_path="${SCRIPT_DIR}/vim/vimrc"
+coc_config_path="${SCRIPT_DIR}/vim/coc-config.vim"
 target_vimrc="$HOME/.vimrc"
 
 # Output source commands to target vimrc
